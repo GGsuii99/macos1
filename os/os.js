2a8ce16d5c7b639347a81995100d81856e5a75df
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-03-14 14:36:46 +0200

    fix: OS public files

--- os/os.js
+++ os/os.js
@@ -5,11 +5,11 @@ import macplus from "pcejs-macplus";
 macplus({
   arguments: ["-c", "pce-config.cfg", "-r"],
   autoloadFiles: [
-    "/macplus-pcex.rom",
-    "/mac-plus-3.rom",
-    "/hd1.qed",
-    "/hd2.img",
-    "/pce-config.cfg",
+    "macplus-pcex.rom",
+    "mac-plus-3.rom",
+    "hd1.qed",
+    "hd2.img",
+    "pce-config.cfg",
   ],
   print: console.log.bind(console),
   printErr: console.warn.bind(console),
